c376b178c7c385dee52de93aab705d0af3e57445
Pedro Rodrigues Chagas <pedro.chagas@aluno.ueg.br>
2025-03-09 14:20:56 -0300

    Implementação do Redis como sistema de cache e retirando do cookies informações sigilosas

--- main.rb
+++ main.rb
@@ -2,6 +2,12 @@ require 'sinatra'
 require 'sqlite3'
 require 'digest'
 require 'json'
+require 'redis'
+require 'securerandom'
+
+
+# docker start redis-stack-server <- para iniciar o redis caso ele não tenha iniciado
+$redis = Redis.new(host: "localhost", port: 6379)
 
 # Criação do banco de dados e criação da tabela usuários
 $database = SQLite3::Database.new "database.db"
@@ -45,23 +51,27 @@ end
 
 set :public_folder, __dir__ + '/static'
 
-
+# Página principal
 get '/' do
-    cookie = request.cookies["user"]
+    uuid = request.cookies["user"]
     
-    unless cookie.nil?
-        @user = JSON.parse(cookie, symbolize_names: true)
+    # verifica se o usuário está logado
+    unless uuid.nil?
+        #@user = JSON.parse(cookie, symbolize_names: true)
+        @user = $redis.hgetall(uuid)
     end
     
     erb :index
 end
 
+# Processamento de dados para o login dos usuários
 get '/login' do
-    cookie = request.cookies["user"]
+    uuid = request.cookies["user"]
     
     # verifica se o usuário está logado
-    unless cookie.nil?
-        @user = JSON.parse(cookie, symbolize_names: true)
+    unless uuid.nil?
+        #@user = JSON.parse(cookie, symbolize_names: true)
+        @user = $redis.hgetall(uuid)
     end
     erb :login
 end
@@ -81,11 +91,24 @@ post '/login' do
         return "erro"
     else
         print @user
-        response.set_cookie("user", value: @user.to_json, expires: Time.now + 3600)
+        #response.set_cookie("user", value: @user.to_json, expires: Time.now + 3600)
+        expire_time = 300
+        uuid = SecureRandom.uuid
+        response.set_cookie("user", value:  uuid, expires: Time.now + expire_time )
+        
+        # user = {id: login[0][0], email: login[0][1], created: login[0][3], username: login[0][4], birthday: login[0][5]}
+        $redis.hset(uuid, "id", @user[:id])
+        $redis.hset(uuid, "email", @user[:email])
+        $redis.hset(uuid, "created", @user[:created])
+        $redis.hset(uuid, "username", @user[:username])
+        $redis.hset(uuid, "birthday", @user[:birthday])
+        $redis.expire(uuid, expire_time)
+
         erb :login
     end
 end
 
+# -------- Processamento de dados para o registro dos usuários
 get '/register' do
     erb :register
 end
@@ -97,7 +120,7 @@ post '/register' do
     senha_1 = params["password-1"]
     senha_2 = params["password-2"]
 
-    puts "email: #{email}, senha1: #{senha_1}, senha2: #{senha_2}, username: #{username}, birthday: #{birthday} "
+    #puts "email: #{email}, senha1: #{senha_1}, senha2: #{senha_2}, username: #{username}, birthday: #{birthday} "
     if senha_1 == senha_2
         
         password_hash = Digest::SHA256.hexdigest(senha_1)
@@ -116,12 +139,14 @@ post '/register' do
     end
 end
 
+# Processamento de dados para o logout do usuário
 get '/logout' do
-    cookie = request.cookies["user"]
+    uuid = request.cookies["user"]
     
     # verifica se o usuário está logado
-    unless cookie.nil?
-        @user = JSON.parse(cookie, symbolize_names: true)
+    unless uuid.nil?
+        #@user = JSON.parse(cookie, symbolize_names: true)
+        @user = $redis.hgetall(uuid)
     end
     erb :logout
 end
@@ -131,3 +156,5 @@ post '/logout' do
     @sucess = true
     erb :logout
 end
+
+$redis.flushall
